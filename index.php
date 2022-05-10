8a49e4a7418d6d06a019de7801e47df18d92ad04
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-05-10 10:41:19 +0530

    write a program to display count 5 to 15

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-i<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,87 +9,10 @@ i<!DOCTYPE html>
 <body>
 <?php
 
-for($i=1 ;$i<=15; $i++)
+for($i=5 ;$i<=15; $i++)
 {
     echo $i."<br>";
 }
-
-// Write a program to perform sum or addition of two numbers in PHP programming.
-
-$num1=10;
-$num2=20;
-echo $sum= $num1+$num2."<br>";
-
-// Write a program to print “Welcome to the PHP World” using some part of the text in variable & some part directly in echo.
-
-$x="to the PhP";
-echo "welcome". $x ." "."World"."<br>";
-
-// Write a program to print 2 php variables using single echo statement.
-
-$first ="hello";
-$second ="sujeet";
-echo $first." ".$second."<br>";
-
-
-// Write a program to check student grade based on the marks using if-else statement.
-
-$marks =rand(1,100);
-if($marks >=60){
-    echo "first Division"."<br>";
-}
-elseif($marks>=45 && $marks<=59){
-    echo "second division"."<br>";
-}
-elseif($marks>=33 && $marks<=44){
-    echo "third division"."<br>";
-}
-else{
-    echo "fail"."<br>";
-}
-
-
-// Write a program to show day of the week (for example: Monday) based on numbers using switch/case statements.
-
-$day=7;
-switch($day)
-{
-    case 1:
-        echo "Monday"."<br>";
-        break;
-    case 2:
-        echo "tuesday"."<br>";
-        break;
-    case 3:
-        echo "wednesday"."<br>";
-        break;
-    case 4:
-        echo "thursday"."<br>";
-        break;
-    case 5:
-        echo "friday"."<br>";
-        break;
-    case 6:
-        echo "saturday"."<br>";
-        break;
-    case 7:
-        echo "sunday"."<br>";
-        break;
-    default:
-        echo "you have entered a wrong number please enter between 1 to 7"."<br>";
-}
-
-
-// Write a program to calculate factorial of a number using for loop in php.
-
-$fact=5;
-for($i=1; $i<5; $i++){
-    $fact*=$i;
-}
-echo $fact."<br>";
-?>
-
-
 
 ?>
 </body>
