763bb2ceca1cf5340efd8752868842fba98fb9a5
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-05-10 19:25:56 +0530

    Write a PHP script to calculate and display average temperature, five lowest and highest temperatures.

--- array.php
+++ array.php
@@ -39,6 +39,13 @@
         echo $u[$i]." ";
     }
     
+    // Write a PHP program to filter out some elements with certain key-names.
+     
+    $array1 =array("c1"=>"Red","c2"=>"Green","c3"=>"White","c4"=>"Black");
+    $array2 =array("c2","c4");
+
+    $result = array_diff_key($array1,array_flip($array2));
+    print_r($result);
 
     ?>
 </body>
