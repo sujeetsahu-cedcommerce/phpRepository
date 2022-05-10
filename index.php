f9885412f892b03b045773c54c3c473afa1c332a
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-05-10 10:04:01 +0530

    just try

---
+++ index.php
@@ -0,0 +1,24 @@
+i<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+// Write a Program to display count, from 5 to 15 using PHP loop as given below.
+for($i=1 ;$i<=15; $i++)
+{
+    echo $i."<br>";
+}
+
+
+
+
+
+?>
+</body>
+</html>
