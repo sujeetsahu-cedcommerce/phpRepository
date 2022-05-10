37bbf415e03937f3b363fc2fb222c6609518fd39
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-05-10 11:03:40 +0530

    Write a program to perform sum or addition of two numbers in PHP programming.

--- index.php
+++ index.php
@@ -13,6 +13,12 @@ for($i=5 ;$i<=15; $i++)
 {
     echo $i."<br>";
 }
+
+// Write a program to perform sum or addition of two numbers in PHP programming.
+
+$num1=10;
+$num2=20;
+echo $sum= $num1+$num2."<br>";
 ?>
 </body>
 </html>
