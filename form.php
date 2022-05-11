0f11a35dcdd3c86ff40775d4d4384d2a16588646
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-05-11 17:25:54 +0530

    Write a simple calculator program in PHP using switch case

--- form.php
+++ form.php
@@ -5,32 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Electricity-Bill</title>
+    <style>
+        .adjust
+        {
+           margin:10px; 
+        }
+    </style>
 </head>
 <body>
-    <form action="form.php" method="post">
+    <!-- <form action="form.php" method="post">
         units :<input type="text" name="units"><br>
         <input type="submit">
-    </form>
-    <?php
-    
+    </form> -->
+   <?php
     $units = $_POST['units'];
-    // echo $units;
-   
     Bill($units);
     function Bill($units){
         $rs = 0;
         if($units >=50){
             $rs = 50*3.50;
             $units = $units -50;
-            //  echo $rs .",".$units."<br>"; 
             if($units >= 100){
                 $rs = $rs+(100*4.0);
                 $units = $units -100;
-                //  echo $rs .",".$units."<br>";
                 if($units >=100){
                     $rs = $rs+($units*5.20);
                     $units = $units -100;
-                    // echo $rs .",".$units."<br>";
+                  
                     if($units >250){
                         $rs =$rs+($units*6.50);
                         $units =0;
@@ -38,23 +39,50 @@
                 }
                 else{
                     $rs=$rs + $units*5.20;
-                    $units = 0;
-                    // echo $rs .",".$units."<br>";
+                    $units = 0;   
                 }
             }
             else{
                 $rs=$rs + $units*4.0;
-                $units = 0;
-                // echo $rs .",".$units."<br>";
+                $units = 0;  
             }
         }
         else
         {
-            $rs= $rs+$units*3.50;
-            // echo $rs .",".$units."<br>";
+            $rs= $rs+$units*3.50;  
         }
-        echo "bill =".$rs;
+        
+    } 
+    ?> 
+    
+    <?php
+    $button=$_POST['button'];
+    $num1=$_POST['number1'];
+    $num2=$_POST['number2'];
+    switch ($button){
+        case'+':
+            $result=$num1 + $num2;
+            break;
+        case'-':
+            $result=$num1 - $num2;
+            break;
+        case'*':
+            $result=$num1 * $num2;
+            break;
+        case'/':
+            $result=$numb1 / $num2;
+            break;
     }
-    ?>
+   
+    ?>  
+    <form action="form.php" method="post">
+        number1:<input type="text" name="number1" class="adjust" ><br>
+        number2:<input type="text" name="number2" class="adjust" ><br>
+        result :<input type="text" name="result"  class="adjust" value="<?php echo $result;?>"><br>
+        <input type="submit" value="+" name="button" class="operator">
+        <input type="submit" value="-" name="button" class="operator">
+        <input type="submit" value="*" name="button" class="operator">
+        <input type="submit" value="/" name="button" class="operator">
+    </form>
 </body>
 </html>
